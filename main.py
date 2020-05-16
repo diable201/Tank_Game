1b588ce0e3b1ae514a01935928cfa6d91d41fe23
Sanzhar <diable201@protonmail.com>
2020-05-17 04:10:38 +0600

    Correct images of bullets.

--- main.py
+++ main.py
@@ -456,6 +456,7 @@ def multiplayer():
                 self.token = self.response['token']
                 self.tank_id = self.response['tankId']
                 self.room_id = self.response['roomId']
+
                 return True
             return False
 
@@ -518,20 +519,22 @@ def multiplayer():
 
         def draw_tank(self, x, y, direction, **kwargs):
             # print(**kwargs)
-
-
             if direction == UP:
                 tank_image = pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/player_1.png')
                 screen.blit(tank_image, (x, y))
+
             if direction == DOWN:
                 tank_image = pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/player_3.png')
                 screen.blit(tank_image, (x, y))
+
             if direction == LEFT:
                 tank_image = pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/player_2.png')
                 screen.blit(tank_image, (x, y))
+
             if direction == RIGHT:
                 tank_image = pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/player_4.png')
                 screen.blit(tank_image, (x, y))
+
 
     UP = 'UP'
     DOWN = 'DOWN'
@@ -547,12 +550,12 @@ def multiplayer():
 
 
     def draw_bullet(x, y, width, height, direction):
-        my_im = pygame.transform.scale(pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/Shell_1.png'), (15,15))
-        screen.blit(my_im, (x, y))
-        # bullet_c = (x + int(width / 2), y + int(width / 2))
-        # pygame.draw.rect(screen, (255, 255, 255),
-        #                  (x, y, width, height))
-        # pygame.draw.rect(screen, (255, 255, 255), bullet_c)
+        if direction == RIGHT or direction == LEFT:
+            my_im = pygame.transform.scale(pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/Shell_1.png'), (15, 5))
+            screen.blit(my_im, (x, y))
+        if direction == UP or direction == DOWN:
+            my_im = pygame.transform.scale(pygame.image.load('/Users/sanzhar/Tank_Game-/Textures/Player_Multiplayer/Shell_2.png'), (5, 15))
+            screen.blit(my_im, (x, y))
 
     class Button:
 
@@ -573,28 +576,27 @@ def multiplayer():
     def click_button():
         print('ok good')
 
-    # tanks = [player]
-    # all_sprites = pygame.sprite.Group()
     player = TankConsumerClient('room-7')
 
     def game_start():
         mainloop = True
-        font = pygame.font.Font('freesansbold.ttf', 32)
+        font = pygame.font.Font('freesansbold.ttf', 16)
         button = Button(100, 100, 100, 100, click_button())
         while mainloop:
             pos = pygame.mouse.get_pos()
             # print(pos)
-            # screen.fill(BLACK)
             screen.fill(BLACK)
             screen.blit(background, background_rect)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     mainloop = False
                     event_client.daemon = True
+                    pygame.quit()
                     client.connection.close()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         mainloop = False
+                        pygame.quit()
                         client.connection.close()
                     if event.key in MOVE_KEYS:
                         client.turn_tank(client.token, MOVE_KEYS[event.key])
@@ -605,13 +607,6 @@ def multiplayer():
                         button.func()
             button.draw()
             try:
-                remaining_time = event_client.response['remainingTime']
-                # health_server = event_client.response['health']
-                text = font.render('Remaining Time: {}'.format(remaining_time), True, (255, 255, 255))
-                textRect = text.get_rect()
-                textRect.center = (400, 100)
-                screen.blit(text, textRect)
-
                 hits = event_client.response['hits']
                 bullets = event_client.response['gameField']['bullets']
                 winner = event_client.response['winners']
@@ -619,26 +614,42 @@ def multiplayer():
 
                 for tank in tanks:
                     player.draw_tank(**tank)
-                    # tank_score = tank['score']
-                    # tank_dict = {tank['score'], tank['health']}
-                tanks_dict = {
-                    "id": tank['id'],
-                    "score": tank['score'],
-                    "health": tank['health']
-                }
+
                 tanks_dict_2 = {
                     "id": tank['id'],
                     "score": tank['score'],
                     "health": tank['health']
                 }
-                text_score = font.render('Id:' + tanks_dict.get('id'), True, (255, 255, 255))
+
+                remaining_time = event_client.response['remainingTime']
+                # health_server = event_client.response['health']
+                text = font.render('Remaining Time: {}'.format(remaining_time), True, WHITE)
                 textRect = text.get_rect()
-                textRect.center = (200, 400)
-                screen.blit(text_score, textRect)
-                text_score_2 = font.render('Id:' + tanks_dict_2.get('id'), True, (255, 255, 255))
-                textRect_2 = text.get_rect()
-                textRect.center = (400, 400)
-                screen.blit(text_score_2, textRect_2)
+                textRect.center = (850, 100)
+                screen.blit(text, textRect)
+
+                my_id = client.tank_id
+                text = font.render('ID: {}'.format(my_id), True, WHITE)
+                textRect = text.get_rect()
+                textRect.center = (850, 150)
+                screen.blit(text, textRect)
+
+                my_score = tank['score']
+                text = font.render('My Scores: ' + str(my_score), True, WHITE)
+                textRect = text.get_rect()
+                textRect.center = (850, 200)
+                screen.blit(text, textRect)
+
+                text = font.render('Not My Scores: {}'.format(tank['score']), True, BLACK)
+                textRect = text.get_rect()
+                textRect.center = (850, 250)
+                screen.blit(text, textRect)
+
+                text = font.render('Lives: {}'.format(tank['health']), True, BLACK)
+                textRect = text.get_rect()
+                textRect.center = (850, 300)
+                screen.blit(text, textRect)
+
                 for bullet in bullets:
                     bullet_x = bullet['x']
                     bullet_y = bullet['y']
@@ -653,7 +664,6 @@ def multiplayer():
         client.connection.close()
         pygame.quit()
 
-
     client = TankRpcClient()
     client.check_server_status()
     client.obtain_token('room-7')
@@ -667,7 +677,7 @@ def start_menu():
     screen.blit(background, background_rect)
     render(screen, "Welcome to Armored Kill", 48, WIDTH / 2, HEIGHT / 4)
     render(screen, "Arrow WASD/arrows to move. Hold Enter/Space to fire", 25, WIDTH / 2, HEIGHT / 1.5)
-    render(screen, "Press any key to begin", 22, WIDTH / 2, HEIGHT * 3 / 4)
+    render(screen, "For SinglePlayer press 1. For MultiPlayer press 2", 22, WIDTH / 2, HEIGHT * 3 / 4)
     pygame.display.flip()
     menu = True
     while menu:
@@ -678,7 +688,7 @@ def start_menu():
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_1]:
                 menu = False
-            if pressed[pygame.K_w]:
+            if pressed[pygame.K_2]:
                 multiplayer()
 
 
@@ -701,7 +711,7 @@ def end_menu():
 def render(surface, text, size, x, y):
     font_name = pygame.font.match_font('Arial')
     font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, BLACK)
+    text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surface.blit(text_surface, text_rect)
